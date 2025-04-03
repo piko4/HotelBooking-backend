ae8ebdf9c3b3d900c3d29ed10964fd0ec424135d
Piyush(piko04) <156587759+piko4@users.noreply.github.com>
2025-04-03 14:18:10 +0530

    Added initials and session based user authentication

---
+++ UserService/src/main/java/com/hb/userservice/UserServiceApplication.java
@@ -0,0 +1,16 @@
+package com.hb.userservice;
+
+import org.hibernate.annotations.processing.Exclude;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
